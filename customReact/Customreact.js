1a855314a9dc7b17e61558559c39418bf405b77c
yashikasoni <yashika.evdt@gmail.com>
2025-08-12 13:46:16 +0530

    second commit

---
+++ customReact/Customreact.js
@@ -0,0 +1,31 @@
+// const { Children } = require("react");
+
+function customRender(reactElement, container) {
+  // const domElement = document.createElement(reactElement.type)
+  // domElement.innerHTML = reactElement.Children
+  // domElement.setAttribute('herf', reactElement.props.herf)
+  // domElement.setAttribute('target', reactElement.props.target)
+
+    // container.appendChild(domElement)
+    
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.Children;
+  for (const prop in reactElement.props) {
+      if (prop === 'Children') continue;
+      domElement.setAttribute (prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    herf: "https://google.com",
+    target: "_blank",
+  },
+  Children: "click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);

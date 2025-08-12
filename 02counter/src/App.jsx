8f62f4486f2827aa26b27c52cb753d743ae310ad
yashikasoni <yashika.evdt@gmail.com>
2025-08-12 16:44:11 +0530

    project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let  [counter, setCounter] = useState(15)
+  
+  // let counter = 15
+
+  const addValue = () => {
+    counter = counter + 1
+    setCounter(counter);
+  }
+  
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1> chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value { counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+       <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;

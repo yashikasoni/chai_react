c33a1b7a96e956147b97609aed75fd20f871ecf7
yashikasoni <yashika.evdt@gmail.com>
2025-08-29 12:34:18 +0530

    Context api project

---
+++ 08ContextAPI/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Login from './component/Login'
+import Profile from './component/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Please login first</h1> 
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
